e4f98d0998f136f3b7aba4d4dd36f6751f2585c7
AngelL327 <ajlagosp@unah.hn>
2025-02-08 00:34:49 -0600

    feat: update Ejercicio-1.dart

--- Tarea1.1_AngelLagos_ByronMolina/Ejercicio-1.dart
+++ Tarea1.1_AngelLagos_ByronMolina/Ejercicio-1.dart
@@ -1,5 +1,5 @@
 void main(){
-  // Ejercicio #1 fibonacci
+  // Ejercicio #1  sucesion fibonacci
 List<int> fibonacci = [0,1];
 for(int i=2 ; i <15 ; i++){
 fibonacci.add(fibonacci[i-1]+ fibonacci[i-2]);
