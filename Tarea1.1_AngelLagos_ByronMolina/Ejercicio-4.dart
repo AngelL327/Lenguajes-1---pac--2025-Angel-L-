b4d628b215501a136ccd8ad956fae5345e836d96
bmolina1 <bmmolina@unah.hn>
2025-02-08 16:09:19 -0600

    feat: update Ejercicio-4.dart

--- Tarea1.1_AngelLagos_ByronMolina/Ejercicio-4.dart
+++ Tarea1.1_AngelLagos_ByronMolina/Ejercicio-4.dart
@@ -48,7 +48,7 @@ void main() {
 
   final libro1 = Libro(titulo: '1984', autor: 'George Orwell', anioPublicacion: 1949);
   final libro2 = Libro(titulo: 'Cien años de soledad', autor: 'Gabriel García Márquez', anioPublicacion: 1967);
-  final libro3 = Libro(titulo: 'El león, la bruja y el armario', autor: ' C.S Lewis ', anioPublicacion: 1950);
+  final libro3 = Libro(titulo: 'El leon, la bruja y el armario', autor: ' C.S Lewis ', anioPublicacion: 1950);
   final libro4 = Libro(titulo: 'Don Quijote de la Mancha', autor: 'Miguel de Cervantes', anioPublicacion: 1605);
 
   biblioteca.agregarLibro(libro1);
